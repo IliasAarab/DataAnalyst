75c59ea9ad77cd92e09809a8fed319451716d64a
Aarab, Ilias <Ilias.Aarab@ecb.europa.eu>
2022-01-08 11:51:19 +0100

    JN env settings

--- data_analyst/utils/set_env.py
+++ data_analyst/utils/set_env.py
@@ -27,7 +27,7 @@ from win32com.client import Dispatch
 import openpyxl
 
 # ECB libs
-from connectors import disc
+# from connectors import disc
 
 # -----------------------------------------------------------
 
